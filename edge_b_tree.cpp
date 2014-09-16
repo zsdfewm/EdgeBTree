db2ea3bd77dadd6b98596dfad4bb4f794ac6719a
Mu Wang <muwang@Mus-MacBook-Pro.local>
2014-09-16 14:10:00 -0400

    push to github

--- edge_b_tree.cpp
+++ edge_b_tree.cpp
@@ -20,24 +20,56 @@ EdgeBTreeBlock::~EdgeBTreeBlock() {
   free(child_block);
 }
 
-EdgeBTreeBlock::EraseBlock() {
+void EdgeBTreeBlock::EraseBlock() {
   int i;
   for(i=0;i<=len;i++) {
-    if (chlid_block[i] != NULL && !is_leaf) {
-      chlid_block[i]->EraseBlock();
-      delete chlid_block[i];
+    if (child_block[i] != NULL && !is_leaf) {
+      child_block[i]->EraseBlock();
+      delete child_block[i];
+    }
+  }
+}
+void EdgeBTreeBlock::AppendToArray(size_t *len_array, locint *ind_array, double *weight_array) {
+  for(size_t i = 0; i < len; i++) {
+    ind_array[*len_array] = ind[i];
+    weight_array[*len_array] = weight[i];
+    *len_array++;
+  }
+  if (!is_leaf) {
+    for(size_t i = 0; i <= len; i++) {
+      child_block[i]->AppendToArray(len_array, ind_array, weight_array);
     }
   }
 }
 
+void EdgeBTreeBlock::PrintBlock() {
+  if (!is_leaf) {
+    for(size_t i = 0; i<=len; i++) {
+      child_block[i]->PrintBlock();
+    }
+  }
+  printf("B[%x]:",this);
+  for(size_t i = 0; i<len; i++) {
+    if (!is_leaf) {
+      printf("%x, ", child_block[i]);
+    }
+    printf("%<%d, %10.5f>, ", ind[i], weight[i]);
+  }
+  if (!is_leaf) {
+    printf("%x", child_block[len]);
+  }
+  printf("\n");
+}
+
 EdgeBTree::EdgeBTree() {
-  root = new EdgeBTreeBlock();
-  root->is_leaf = true;
+  root_block = new EdgeBTreeBlock();
+  root_block->is_leaf = true;
+  size = 0;
 }
 
 EdgeBTree::~EdgeBTree() {
-  root->EraseBlock();
-  delete root;
+  root_block->EraseBlock();
+  delete root_block;
 }
 
 void EdgeBTree::update(locint index, double w) {
@@ -53,49 +85,49 @@ void EdgeBTree::erase(locint index) {
 double* EdgeBTree::find(locint index) {
   EdgeBTreeBlock *prev_block = NULL;
   size_t prev_ind = 0;
-  EdgeBTreeBlock *curr_block = root;
+  EdgeBTreeBlock *curr_block = root_block;
 
-  while () {
+  while (true) {
     if (curr_block->len == EdgeBTreeBlockSize) {
       //split two blocks;
       EdgeBTreeBlock *new_block = new EdgeBTreeBlock();
       size_t i,j;
-      for(i = EdgeBTreeSizeT+1, j=0; i < EdgeBTreeSize; ++i, ++j) {
+      for(i = EdgeBTreeBlockSizeT+1, j=0; i < EdgeBTreeBlockSize; ++i, ++j) {
         new_block->ind[j] = curr_block->ind[i];
         curr_block->ind[i] = 0;
         new_block->weight[j] = curr_block->weight[i];
         curr_block->weight[i] = 0.0;
-        new_block->child_block[j] = curr_block->chlid_block[i];
+        new_block->child_block[j] = curr_block->child_block[i];
         curr_block->child_block[i] = NULL;
       }
-      new_block->chlid_block[EdgeBTreeSizeT] = curr_block->child_block[EdgeBTreeSize];
-      curr_block->child_block[EdgeBTreeSize] = NULL;
+      new_block->child_block[EdgeBTreeBlockSizeT] = curr_block->child_block[EdgeBTreeBlockSize];
+      curr_block->child_block[EdgeBTreeBlockSize] = NULL;
       new_block->is_leaf = curr_block->is_leaf;
 
-      new_block->len = EdgeBTreeSize;
-      curr_block->len = EdgeBTreeSize;
+      new_block->len = EdgeBTreeBlockSizeT;
+      curr_block->len = EdgeBTreeBlockSizeT;
 
       // put mid to prev block
-      locint mid_ind = curr_block->ind[EdgeBTreeSizeT];
-      curr_block->ind[EdgeBTreeSizeT] = 0;
-      double mid_weight = curr_block->weight[EdgeBTreeSizeT];
-      curr_block->weight[EdgeBTreeSizeT] = 0.0;
+      locint mid_ind = curr_block->ind[EdgeBTreeBlockSizeT];
+      curr_block->ind[EdgeBTreeBlockSizeT] = 0;
+      double mid_weight = curr_block->weight[EdgeBTreeBlockSizeT];
+      curr_block->weight[EdgeBTreeBlockSizeT] = 0.0;
 
       if (prev_block = NULL) {
         //Build a new root;
-        root = new EdgeBTreeBlock();
-        root->ind[0] = mid_ind;
-        root->weight[0] = mid_weight;
-        root->child_block[0] = curr_block;
-        root->chile_block[1] = new_block;
-        root->len = 1;
+        root_block = new EdgeBTreeBlock();
+        root_block->ind[0] = mid_ind;
+        root_block->weight[0] = mid_weight;
+        root_block->child_block[0] = curr_block;
+        root_block->chile_block[1] = new_block;
+        root_block->len = 1;
       } else {
         size_t i;
         prev_block->len++;
         for(i = prev_block->len; i > prev_ind; --i) {
           prev_block->ind[i]=prev_block->ind[i-1];
           prev_block->weight[i]=prev_block->weight[i-1];
-          prev_block->chlid_block[i+1] = prev_block->child_block[i];
+          prev_block->child_block[i+1] = prev_block->child_block[i];
         }
         prev_block->ind[prev_ind] = mid_ind;
         prev_block->weight[prev_ind] = mid_weight;
@@ -123,6 +155,9 @@ double* EdgeBTree::find(locint index) {
       }
       curr_block->ind[prev_ind] = index;
       curr_block->weight[prev_ind] = 0.0;
+
+      this->size++;
+
       return &(curr_block->weight[prev_ind]);
     } else {
       prev_block = curr_block;
@@ -131,7 +166,27 @@ double* EdgeBTree::find(locint index) {
   }
 }
 
+void EdgeBTree::ToArray(size_t *len, locint **ind, double **weight) {
+
+  (*ind) = (locint*) malloc(sizeof(locint) * size);
+  (*weight) = (locint*) malloc(sizeof(locint) * size);
+  *len=0;
+  root->AppendToArray(len, *ind, *weight);
+  if (*len != size) {
+    fprintf(stderr, "FATAL ERROR: EdgeBTree Broken!\n");
+    exit(-1);
+  }
+}
+
+void EdgeBTree::Print() {
+  root_block->PrintBlock();
+}
+
 int main() {
   //test edge_b_tree;
-
+  EdgeBTree *edge_b_tree = new EdgeBTree();
+  for(size_t i=0; i<10;i++) {
+    edge_b_tree->update(i,-1.0*i);
+  }
+  edge_b_tree->Print();
 }
--- edge_b_tree.h
+++ edge_b_tree.h
@@ -5,26 +5,47 @@ class EdgeBTreeBlock {
  public:
   // default ctor
   EdgeBTreeBlock();
+  
+  // d-tor
+  ~EdgeBTreeBlock();
 
- private:
+  // AppendToArray
+  void AppendToArray(size_t *len_array, locint *ind_array, double *weight_array);
+
+  // PrintBlock
+  void PrintBlock();
+
+  void EraseBlock();
+
+  size_t len;
+  bool is_leaf;
   locint *ind;
   double *weight;
-  EdgeBTreeBlock *child_block;
-}
+  EdgeBTreeBlock **child_block;
+};
 
 class EdgeBTree {
  public:
   // default ctor, initialize the root_block;
   EdgeBTree();
 
+  // dtor
+  ~EdgeBTree();
+
   // update an entry, for non-existing entries, create one with 0.0
-  void update(int i, double w);
+  void update(locint index, double w);
 
   // find/create an entry, return the reference to the weight location;
-  double* find(int i);
+  double* find(locint index);
 
+  void erase(locint index);
+
+  void Print();
+
+  void ToArray(size_t *len_array, locint **ind_array, double **weight_array);
 
  private:
-  EdgeBTreBlock *root_block;
+  EdgeBTreeBlock *root_block;
+  size_t size;
 
-}
+};
